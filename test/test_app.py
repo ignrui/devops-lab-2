933eb4b454699643711c1e9e1719ed80ee0808a4
IGNACIO RUIZ CASTILLO <ignacio.ruiz@ctag.com>
2025-11-10 08:49:46 +0000

    ci: add pytest to requirements and fix test import path

--- test/test_app.py
+++ test/test_app.py
@@ -1,12 +1,12 @@
 """
 Basic tests for the Flask application
 """
-from app import app
-import unittest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
+import unittest
+from app import app
 
 
 class TestFlaskApp(unittest.TestCase):
